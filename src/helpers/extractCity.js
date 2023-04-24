5f6c07d277307d32594e613719353d385ab48e5b
Nikodem Domaracki <56053287+Nikkoro@users.noreply.github.com>
2023-04-24 17:01:35 +0200

    chore: remove comments

--- src/helpers/extractCity.js
+++ src/helpers/extractCity.js
@@ -58,11 +58,4 @@ function matchCityName(input) {
   return match;
 }
 
-// const input =
-//   "Edo € 5.0752M 5.0752M Our establishment offers fair trade to 0 / 200k all: You can trust our 01.59.07 services! Gold Leather Price 862 (96%) Price 194 (97%) Weight 75 Weight 16 5.1K 14.45K Cloth Peanuts Price 247 (99%f) Price 110 (100%) Weight 22 Weight 10 12.24K 21.624K Bananas Agate Price 50 (100%) Price 1.006K (101%) Weight 4 Weight 100 37.4K 4.76K Meat Tobacco Price 92 (102%) Price 513 (103%) Weight 6 Weight 50 24.548K 82K Porcelain Tea Leaves Price 832 (104%) Price 481 (107%) Weight 57 Weight 37 5.304K 8.296K Alcohol Price 728 (86%) Weight 65 Sold out Purchase Sell";
-// const matchedCity = matchCityName(input.substring(0, 20));
-
-// console.log(
-//   `Matched city for input "${input.substring(0, 50)}" is "${matchedCity}".`
-// );
 module.exports = matchCityName;
