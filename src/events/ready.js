4e27ad44b740d66933185fc33669697de895ee35
nikkoro <nikodemdomaracki@gmail.com>
2023-11-28 17:18:24 +0100

    feat: :sparkles: Added bot activity status

--- src/events/ready.js
+++ src/events/ready.js
@@ -4,6 +4,7 @@ module.exports = {
   name: Events.ClientReady,
   once: true,
   execute(client) {
+    client.user.setActivity("Sailing the seas 🦜🏴‍☠️⛵");
     console.log(`Ready! 😎 Logged in as ${client.user.tag}`);
   },
 };
