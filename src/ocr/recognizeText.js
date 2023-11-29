02a8f28c252363f5479cf9bb2bb8035de09a2968
nikkoro <nikodemdomaracki@gmail.com>
2023-11-29 02:33:28 +0100

    feat: :sparkles: changed page segmentation mode for better text readibility
    Changed page segmentation mode & created character whitelist

--- src/ocr/recognizeText.js
+++ src/ocr/recognizeText.js
@@ -1,11 +1,17 @@
 const { createWorker } = require("tesseract.js");
 
 const recognizeText = async (imgBuffer) => {
-  const worker = await createWorker({
-    logger: (m) => console.log(m),
+  const worker = await createWorker("eng2+osd", {
+    // logger: (m) => console.log(m),
   });
-  await worker.loadLanguage("eng2");
-  await worker.initialize("eng2");
+
+  await worker.setParameters({
+    tessedit_pageseg_mode: "1",
+  });
+  await worker.setParameters({
+    tessedit_char_whitelist:
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789%() ",
+  });
 
   const {
     data: { text },
